dd0e2c659ab4d606ae041d984ccd7dc08a85ac80
DK <thedavidkipnis@gmail.com>
2024-11-07 12:48:55 -0800

    Added notes for functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import Navbar from './Navbar'
 import Board from './Board'
 
+/*
+To install react: Install node.js, run npx create-react-app tts in cmd
+To open VSCode in a directory from cmd: navigate to directory, run "code ."
+If node_modules is missing: run "install npm", this will look at the package.json folder and install everything from the dependencies
+
+Core functionality:
+- add a sticky (either by clicking on screen or + button on nav bar)
+- erase all (nav bar) (with confirmation, i.e. "are you sure you want to delete all your notes?")
+
+*/
+
+
 function App() {
   return (
     <div className="App">
